0b6b3309dfb79b34b4cd006fe7499ef5a377d3c1
rifqifinaldy <rifqifinaldy@gmail.com>
2022-03-21 20:14:36 +0700

    Testing Detail Page

--- pages/list/index.js
+++ pages/list/index.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
-const index = () => {
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  return {
+    props: { users: data },
+  };
+};
+
+const index = ({ users }) => {
   return (
-    <div>index</div>
-  )
-}
+    <div>
+      <h1>User Index</h1>
+      {users.map((user) => (
+        <Link href={'/list/' + user.id} key={user.id}>
+          <a>
+            <h3>{user.name}</h3>
+          </a>
+        </Link>
+      ))}
+    </div>
+  );
+};
 
-export default index
+export default index;
