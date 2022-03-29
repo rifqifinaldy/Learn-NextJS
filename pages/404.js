ea4d48f96c3d6b21663c622c32cb6a0c758c9697
rifqifinaldy <rifqifinaldy@gmail.com>
2022-03-29 21:09:14 +0700

    404 Functionality

--- pages/404.js
+++ pages/404.js
@@ -1,30 +1,56 @@
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
+import { Container, Typography, Link as MUILink } from "@mui/material";
+import { Box } from "@mui/system";
 
 const NotFound = () => {
-    const router = useRouter();
-    const [counter, setCounter] = useState(3);
+  const router = useRouter();
+  const [counter, setCounter] = useState(3);
 
-    setInterval(() => {
-        setCounter(counter-1)
-    }, 1000);
+  setInterval(() => {
+    setCounter(counter - 1);
+  }, 1000);
 
-    useEffect(() => {
-      setTimeout(() => {
-          router.push('/')
-      }, 3000);
-    }, [router])
-    
+  useEffect(() => {
+    setTimeout(() => {
+      router.push("/");
+    }, 30000);
+  }, [router]);
 
   return (
-    <div>
-      <h1>OOOPS........</h1>
-      <h2>That Page Can&apos;t be found</h2>
-      <p>
-        You will be redirected to the <Link href="/">Home Page</Link> in {counter}
-      </p>
-    </div>
+    <Container>
+      <Box
+        sx={{
+          height: "80vh",
+          position: "relative",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Box
+          sx={{
+            position: "absolute",
+            top: "40%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            textAlign: "center",
+          }}
+        >
+          <Typography color="error" variant="h1">
+            OOOPS
+          </Typography>
+          <Typography>That Page Can&apos;t be found</Typography>
+          <Typography>
+            You will be redirected to the
+            <Link href="/">
+              <MUILink> Home Page </MUILink>
+            </Link>{" "}
+            in {counter}
+          </Typography>
+        </Box>
+      </Box>
+    </Container>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,8 @@ import { Button, Container, Typography } from "@mui/material";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
-import PageTitle from "../components/PageTitle/PageTitle";
 import { useStyles } from "../styles/home-style";
-import Navbar from "../components/Navbar/Navbar"
+import Navbar from "../components/Navbar/Navbar";
 
 export default function Home() {
   const classes = useStyles();
@@ -21,12 +20,12 @@ export default function Home() {
           priority="lazy"
         />
         <div className={classes.welcome}>
-          <Typography variant="h1">
-            Pre-Storic
-          </Typography>
-          <Button color="success" variant="outlined">
-            Explore Now
-          </Button>
+          <Typography variant="h1">Pre-Storic</Typography>
+          <Link href="/dino">
+            <Button color="success" variant="outlined">
+              Explore Now
+            </Button>
+          </Link>
         </div>
       </div>
     </div>
