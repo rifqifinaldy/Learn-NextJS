109099d804e3261b5c4fe1571fc47e1729274b26
rifqifinaldy <rifqifinaldy@gmail.com>
2022-03-29 23:02:42 +0700

    fix production issue 2

--- pages/about/aboutMock.js
+++
@@ -1,6 +0,0 @@
-export const techDesc = [
-    {
-        tech: 'ReactJS',
-        description : "React JS is my favorite Javascript Library, it's help me a lot to design reusable component"
-    }
-]
